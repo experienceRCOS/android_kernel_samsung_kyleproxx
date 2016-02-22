a2caf6c32c10e83f2f0b8f2a9accc1271fede038
Chris Brand <chris.brand@broadcom.com>
2016-02-22 09:17:21 +0000

    Kona PM: Check value returned by clk_enable()
    Samsung identified that handle_ahb_reg_parm() failed to check the
    value returned by clk_enable(). Fix that.
    
    Change-Id: I77a91cb80497f82885e286ee5f38e361b7649692
    Signed-off-by: Chris Brand <chris.brand@broadcom.com>
    Reviewed-on: http://mps-gerrit.broadcom.com/51890
    Branch-Open: Branch Status <branch_status_noreply@broadcom.com>
    Reviewed-by: Commit Description Check <commit_desc_noreply@broadcom.com>
    Reviewed-by: Checkpatch Status <checkpatch_status_noreply@broadcom.com>
    Reviewed-by: Vikram Prakash <vikramp@broadcom.com>
    Tested-by: AutoSubmit Status <autosubmit_status_noreply@broadcom.com>

--- arch/arm/plat-kona/kona_pm_dbg.c
+++ arch/arm/plat-kona/kona_pm_dbg.c
@@ -345,7 +345,8 @@ static u32 handle_ahb_reg_parm(struct snapshot *s)
 	if (IS_ERR_OR_NULL(clk))
 		goto err;
 
-	clk_enable(clk);
+	if (clk_enable(clk))
+		goto err;
 	ret = readl(s->reg) & s->mask;
 	clk_disable(clk);
 
