d1a89585fcdb34f2c3f6b8f73281e0e605830bfa
Shameer Puthalan <shameer@broadcom.com>
2016-02-22 09:23:31 +0000

    Suspend: Moved AP sleep flag ON/OFF to pm_notifier
    [Defect / Enhancement / New Feature]
    
    [Problem]
    AP fails to enter suspend during 3G voice call
    if CP logs are enabled
    
    [Solution]
    For 3.10 kernel, suspend will be immediately aborted and freezing
    of tasks will fail if a wake lock gets locked while AP is
    entering suspend. For AP to enter suspend, we should make sure the
    AP sleep flag is enabled (to disable CP logs) and no ipc wake lock
    gets enabled due to CP logs once suspend is initiated.
    
    Moved AP sleep flag ON/OFF to pm suspend notifier to make sure that
    the flag is turned off as soon as suspend is initiated
    
    [Reviewers]
    Inder, Sandeep
    
    [Testing]
        [Test Cases]
    
        [Label/CL# Synced To]
    
        [Platform]
    Hawaii, Java
    
    [BROADCOM INTERNAL NOTES]
    
    Change-Id: I4181bcddf118cc30c524d85ea2152281692972c8
    Signed-off-by: Shameer Puthalan <shameer@broadcom.com>
    Reviewed-on: http://mps-gerrit.broadcom.com/53692
    Branch-Open: Branch Status <branch_status_noreply@broadcom.com>
    Reviewed-by: Commit Description Check <commit_desc_noreply@broadcom.com>
    Reviewed-by: Inder (Inderpal) Saluja <isaluja@broadcom.com>
    Tested-by: AutoSubmit Status <autosubmit_status_noreply@broadcom.com>

--- arch/arm/plat-kona/kona_pm.c
+++ arch/arm/plat-kona/kona_pm.c
@@ -65,6 +65,7 @@ struct kona_pm_params {
 	u32 suspend_state;
 	int log_lvl;
 	spinlock_t cstate_lock;
+	struct notifier_block pm_notifier;
 };
 
 static struct kona_pm_params pm_prms = {
@@ -175,6 +176,30 @@ struct cpuidle_driver kona_idle_driver = {
 
 #ifdef CONFIG_SUSPEND
 
+static int kona_pm_notifier(struct notifier_block *notifier,
+		       unsigned long pm_event,
+		       void *unused)
+{
+	switch (pm_event) {
+	case PM_SUSPEND_PREPARE:
+#ifdef CONFIG_BCM_MODEM
+		BcmRpc_SetApSleep(1);
+#endif
+		break;
+
+	case PM_POST_SUSPEND:
+#ifdef CONFIG_BCM_MODEM
+		BcmRpc_SetApSleep(0);
+#endif
+		break;
+
+	default:
+		break;
+	}
+	return NOTIFY_DONE;
+}
+
+
 __weak int kona_mach_pm_begin(suspend_state_t state)
 {
 	if (LOG_LEVEL_ENABLED(KONA_PM_LOG_LVL_FLOW))
@@ -226,9 +251,7 @@ __weak int kona_mach_pm_enter(suspend_state_t state)
 		if (suspend->enter) {
 			pr_info("--%s:suspend->enter--\n", __func__);
 
-#ifdef CONFIG_BCM_MODEM
 			BcmRpc_SetApSleep(1);
-#endif
 
 #ifdef CONFIG_KONA_PROFILER
 			err = start_profiler("ccu_root",
@@ -256,9 +279,7 @@ __weak int kona_mach_pm_enter(suspend_state_t state)
 			}
 #endif /*CONFIG_KONA_PROFILER*/
 
-#ifdef CONFIG_BCM_MODEM
 			BcmRpc_SetApSleep(0);
-#endif
 		} else
 			cpu_do_idle();
 		break;
@@ -416,6 +437,9 @@ int __init kona_pm_init(struct pm_init_param *ip)
 #ifdef CONFIG_SUSPEND
 	pr_info("--%s : registering suspend hanlders\n", __func__);
 	suspend_set_ops(&kona_pm_ops);
+	pm_prms.pm_notifier.notifier_call = kona_pm_notifier;
+	register_pm_notifier(&pm_prms.pm_notifier);
+
 #endif /*CONFIG_SUSPEND */
 
 	return 0;
