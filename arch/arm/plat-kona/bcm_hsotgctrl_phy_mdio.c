426651bc4b9f2f41dee049ea122c9a2ddedba293
Chris Brand <chris.brand@broadcom.com>
2016-02-22 09:19:12 +0000

    Kona MDIO: Check value returned by clk_enable()
    Samsung identified that bcm_hsotgctrl_phy_mdio_write() and
    bcm_hsotgctrl_phy_mdio_read() failed to check the value returned
    by clk_enable(). Fix that.
    I also changed the value returned by bcm_hsotgctrl_phy_mdio_write().
    It previously returned the value written when successful. It now
    returns zero instead, making it possible to distinguish errors from
    success. Note that no caller surrently checks the value returned anyway,
    so this is both useless and benign.
    bcm_hsotgctrl_phy_mdio_read() returns either a negative errno code or
    the value read, with no way to distinguish between the two. I didn't
    fix that but merely continued it. No caller checks whether it returns
    an error, but I suspect that the first two error conditions may be
    impossible anyway.
    
    Change-Id: I74c7673a4b5817f7d4e69546e5d8b159ee9251f9
    Signed-off-by: Chris Brand <chris.brand@broadcom.com>
    Reviewed-on: http://mps-gerrit.broadcom.com/51898
    Branch-Open: Branch Status <branch_status_noreply@broadcom.com>
    Reviewed-by: Commit Description Check <commit_desc_noreply@broadcom.com>
    Reviewed-by: Checkpatch Status <checkpatch_status_noreply@broadcom.com>
    Reviewed-by: Vikram Prakash <vikramp@broadcom.com>
    Tested-by: AutoSubmit Status <autosubmit_status_noreply@broadcom.com>

--- arch/arm/plat-kona/bcm_hsotgctrl_phy_mdio.c
+++ arch/arm/plat-kona/bcm_hsotgctrl_phy_mdio.c
@@ -178,7 +178,9 @@ int bcm_hsotgctrl_phy_mdio_read(int mdio_num)
 		return -EIO;
 
 	/* Enable mdio clk */
-	clk_enable(bcm_hsotgctrl_handle->mdio_master_clk);
+	val = clk_enable(bcm_hsotgctrl_handle->mdio_master_clk);
+	if (val)
+		return val;
 
 	/* Program necessary values */
 	val = (CHIPREG_MDIO_CTRL_ADDR_WRDATA_MDIO_SM_SEL_MASK
@@ -226,7 +228,9 @@ int bcm_hsotgctrl_phy_mdio_write(int mdio_num, int value)
 		return -EIO;
 
 	/* Enable mdio clk */
-	clk_enable(bcm_hsotgctrl_handle->mdio_master_clk);
+	val = clk_enable(bcm_hsotgctrl_handle->mdio_master_clk);
+	if (val)
+		return val;
 
 	/* Program necessary values */
 	val = (CHIPREG_MDIO_CTRL_ADDR_WRDATA_MDIO_SM_SEL_MASK
@@ -265,7 +269,7 @@ int bcm_hsotgctrl_phy_mdio_write(int mdio_num, int value)
 	/* Disable mdio clk */
 	clk_disable(bcm_hsotgctrl_handle->mdio_master_clk);
 
-	return val;
+	return 0;
 }
 
 
